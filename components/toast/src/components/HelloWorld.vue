caf5e8e2113ae44f438672ca5400957aeee088b6
mingregister <774262573@qq.com>
2020-12-22 00:48:23 +0800

    toast组件

---
+++ components/toast/src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello" @click="show">
+    <button>
+      asdf
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+  methods: {
+    show() {
+      this.$toast.show()
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+</style>
